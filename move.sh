42e0022131b4e8cc0f3a16edc4200d632a2a6ac3
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-03-26 17:56:18 +0300

    adding bash script to copy sounds

---
+++ move.sh
@@ -0,0 +1,4 @@
+cp src/sound/inecraft_death.mp3 dist/assets
+cp src/sound/inecraft_eating.mp3 dist/assets
+cp src/sound/inecraft_levelu.mp3 dist/assets
+cp src/sound/inecraft_zombie_.mp3 dist/assets
