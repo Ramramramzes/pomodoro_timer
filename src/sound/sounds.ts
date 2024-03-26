363f49ac2510ec602f9a811acd97b36248440ff7
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-03-26 17:46:29 +0300

    path_fixed

--- src/sound/sounds.ts
+++ src/sound/sounds.ts
@@ -1,23 +1,23 @@
 export const startSound = () => {
-  const signal = new Audio('src/sound/inecraft_zombie_.mp3');
+  const signal = new Audio('./assets/inecraft_zombie_.mp3');
   signal.volume = 1;
   return signal.play();
 }
 
 export const breakSound = () => {
-  const signal = new Audio('src/sound/inecraft_eating.mp3');
+  const signal = new Audio('./assets/inecraft_eating.mp3');
   signal.volume = 1;
   return signal.play();
 }
 
 export const bigBreakSound = () => {
-  const signal = new Audio('src/sound/inecraft_levelu.mp3');
+  const signal = new Audio('./assets/inecraft_levelu.mp3');
   signal.volume = .3;
   return signal.play();
 }
 
 export const endSound = () => {
-  const signal = new Audio('src/sound/inecraft_death.mp3');
+  const signal = new Audio('./assets/inecraft_death.mp3');
   signal.volume = 1;
   return signal.play();
 }
