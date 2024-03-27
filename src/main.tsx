87a8042abc32421cc6c2b68aac1d929c622d98c9
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-03-27 12:35:46 +0300

    after reload save state

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { store } from './states/store.ts'
+import { store, persistor } from './states/store.ts'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={'loading'} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/states/store.ts
+++ src/states/store.ts
@@ -1,17 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import taskListReducer from "./taskSlice"
 import inputChangeReducer from "./taskInputSlice"
 import activChangeReducer from "./activchange";
 import timerReducer from "./timer"
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  taskList:taskListReducer,
+  inputChange: inputChangeReducer,
+  activeChange: activChangeReducer,
+  timer: timerReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 
 export const store = configureStore({
-  reducer:{
-    taskList:taskListReducer,
-    inputChange: inputChangeReducer,
-    activeChange: activChangeReducer,
-    timer: timerReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
