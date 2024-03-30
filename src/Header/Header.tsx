399ee1f3d67279fb7a70868dfb29e3d773914932
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-03-30 11:08:36 +0300

    fix msg mob darkmode + mob header icons

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -10,16 +10,16 @@ export function Header() {
   const dispatch = useDispatch<AppDispatch>();
   const hoverStates = useSelector((state: RootState) => state.hoverState.hover)
   const darkmode = useSelector((state: RootState) => state.darkmode.darkmode)
-
+  const screenWidth = window.innerWidth;
   return (
     <div className={styles.header}>
-      <Logo width={205} />
+      <Logo width={screenWidth > 500 ? 205 : 100} />
       <div className={styles.btns}>
-        {darkmode ? <button className={styles.darkmode_btn} onClick={() => dispatch(darkmodeState())}><Moon size={25} color={true}/></button> : <button className={styles.darkmode_btn} onClick={() => dispatch(darkmodeState())}><Sun size={30} color={true}/></button>}
+        {darkmode ? <button className={styles.darkmode_btn} onClick={() => dispatch(darkmodeState())}><Moon size={screenWidth > 500 ? 25 : 20} color={true}/></button> : <button className={styles.darkmode_btn} onClick={() => dispatch(darkmodeState())}><Sun size={screenWidth > 500 ? 30 : 25} color={true}/></button>}
         <button
         onMouseEnter={() => dispatch(setStatisticHover())}
         onMouseLeave={() => dispatch(setStatisticHover())}
-        className={styles.stat_btn}><Statistic size={24} color={hoverStates.statistic}/> Статистика</button>
+        className={styles.stat_btn}><Statistic size={screenWidth > 500 ? 24 : 20} color={hoverStates.statistic}/> Статистика</button>
       </div>
     </div>
   );
